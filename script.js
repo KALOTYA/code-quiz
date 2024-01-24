5a61c380662febcda9bdbe6d6f079d16613cd0c3
Ajay Kaloty <kalotya99@gmail.com>
2024-01-23 23:01:44 -0800

    ade changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 console.log("hello world");
-var questionEl = document.getElementById("question-div");
+var questionEl = document.querySelector(".code__quiz__questions");
 
 questionEl.textContent = "What is the meaning of life?"
