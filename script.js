4655beb368b3b7f4d195c25a94d492b064bec9cb
Ajay Kaloty <kalotya99@gmail.com>
2024-02-12 15:38:06 -0800

    Added updated questions to the questions variable

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ var questions = [
         answer: "DOM Traversal"
     },
     {
-        question: "What is navigating the DOM called?",
-        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
-        answer: "DOM Traversal"
+        question: "Arrays in JavaScript can be used to store",
+        options: ["Numbers and Strings", "Booleans", "All of the above"],
+        answer: "All of the above"
     },
     {
-        question: "What is navigating the DOM called?",
-        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
-        answer: "DOM Traversal"
+        question: "The condition in an IF / Else statement is enclosed with...",
+        options: ["Quotes", "Curly Brackets", "Parenthesis"],
+        answer: "Parenthesis"
     },
     {
-        question: "What is navigating the DOM called?",
-        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
-        answer: "DOM Traversal"
+        question: "Commonly used data types DO NOT include:",
+        options: ["Strings", "Booleans", "Alerts"],
+        answer: "Alerts"
     },
     {
-        question: "What is navigating the DOM called?",
-        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
-        answer: "DOM Traversal"
+        question: "String values must be enclosed within ___ when being assigned to variables",
+        options: ["Quotes", "Commas", "Question Marks"],
+        answer: "Quotes"
     },
     {
-        question: "What is navigating the DOM called?",
-        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
-        answer: "DOM Traversal"
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["Javascript", "Terminal/Bash", "console.log"],
+        answer: "console.log"
     },
 ]
 
