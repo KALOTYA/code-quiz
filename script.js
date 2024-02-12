6ab41a33c00ca993c6abecc495776d3ef876f767
Ajay Kaloty <kalotya99@gmail.com>
2024-02-12 15:20:38 -0800

    Added questions variable and syntax for questions

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 console.log("hello world");
+
+var questions = [
+    {
+        question: "What is navigating the DOM called?",
+        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
+        answer: "DOM Traversal"
+    },
+    {
+        question: "What is navigating the DOM called?",
+        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
+        answer: "DOM Traversal"
+    },
+    {
+        question: "What is navigating the DOM called?",
+        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
+        answer: "DOM Traversal"
+    },
+    {
+        question: "What is navigating the DOM called?",
+        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
+        answer: "DOM Traversal"
+    },
+    {
+        question: "What is navigating the DOM called?",
+        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
+        answer: "DOM Traversal"
+    },
+    {
+        question: "What is navigating the DOM called?",
+        options: ["DOM Traversal", "DOM Manipulation", "DOM Access"],
+        answer: "DOM Traversal"
+    },
+]
+
+
+
 var questionEl = document.querySelector(".code__quiz__questions");
 
 questionEl.textContent = "What is the meaning of life?"
